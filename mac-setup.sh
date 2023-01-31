c744b42498628337dafe1145fbc68791360827d6
Luan Bitar <7@lbitar.com>
2023-01-31 01:08:37 -0300

    feat: adding gpg config

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,14 +2,46 @@ HOME="/Users/"$(whoami)
 
 # homebrew
 if ! [ -x "$(command -v brew)" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo '# Set PATH, MANPATH, etc., for Homebrew.' >> "$HOME/.zshrc"
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "HOME/.zshrc"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    echo "brew installed successfully" >&2
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo '# Set PATH, MANPATH, etc., for Homebrew.' >> "$HOME/.zshrc"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zshrc"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  brew install --cask visual-studio-code iterm2
+  brew install git neovim
+  echo "brew installed successfully" >&2
 else
-    echo "brew already installed, skipped" >&2
+  echo "brew already installed, skipped" >&2
 fi
 
-brew install --cask visual-studio-code
-brew install git gnupg
+# gpg
+# following guide: https://gist.github.com/troyfontaine/18c9146295168ee9ca2b30c00bd1b41e
+if ! [ -d "$HOME/.gnupg" ]; then
+  brew install gnupg gpg2 pinentry-mac
+  mkdir ~/.gnupg
+  echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
+  echo "use-agent" > ~/.gnupg/gpg.conf
+  echo 'export GPG_TTY=$(tty)' >> "$HOME/.zshrc"
+  chmod 700 ~/.gnupg
+  killall gpg-agent
+  git config --global gpg.program $(which gpg)
+  echo "gpg installed successfully" >&2
+else
+   echo "gpg already installed, skipped" >&2
+fi
+
+# p10k
+if ! [ -d "$HOME/.zsh" ]; then
+  brew tap homebrew/cask-fonts
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+  brew install font-meslo-lg-nerd-font
+  echo 'source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+  echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+  echo 'HISTFILE=~/.zsh/.zsh_history' >> ~/.zshrc
+  echo 'HISTSIZE=10000' >> ~/.zshrc
+  echo 'SAVEHIST=10000' >> ~/.zshrc
+  echo 'setopt appendhistory' >> ~/.zshrc
+  echo "p10k installed successfully" >&2
+else
+   echo "p10k already installed, skipped" >&2
+fi
+
