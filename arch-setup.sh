d3fbe65e2bd45bac5ce3bafa973c52870c6ec70b
Luan Bitar <7@lbitar.com>
2023-01-31 01:37:42 -0300

    arch: installing packages just inside yay installation

--- arch-setup.sh
+++ arch-setup.sh
@@ -6,84 +6,75 @@ HOME="/home/"$(whoami)
 
 # git
 if ! [ -x "$(command -v git)" ]; then
-    sudo pacman -S --needed --noconfirm git base-devel
-    clear
-    echo "git installed successfully" >&2
+  sudo pacman -S --needed --noconfirm git base-devel
+  clear
+  echo "git installed successfully" >&2
 else
-    echo "git already installed, skipped" >&2
+  echo "git already installed, skipped" >&2
 fi
 
 # yay
 if ! [ -x "$(command -v yay)" ]; then
-    cd /tmp
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf yay
-    cd
-    echo "yay installed successfully" >&2
+  cd /tmp
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+  cd ..
+  rm -rf yay
+  cd
+
+  # common apps
+  yay -S --noconfirm google-chrome notion-app 1password whatsapp-nativefier zoom slack-desktop
+
+  # dev apps
+  yay -S --noconfirm visual-studio-code-bin alacritty neovim
+  echo "yay installed successfully" >&2
 else
-    echo "yay already installed, skipped" >&2
+  echo "yay already installed, skipped" >&2
 fi
 
 # pw10k
 if ! [ -d "$HOME/.zsh" ]; then
-    yay -S --noconfirm zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k powerline-fonts awesome-terminal-fonts
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-    echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-    echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
-    echo 'HISTFILE=~/.zsh/.zsh_history' >> ~/.zshrc
-    echo 'HISTSIZE=10000' >> ~/.zshrc
-    echo 'SAVEHIST=10000' >> ~/.zshrc
-    echo 'setopt appendhistory' >> ~/.zshrc
+  yay -S --noconfirm zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k powerline-fonts awesome-terminal-fonts
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+  echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+  echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+  echo 'HISTFILE=~/.zsh/.zsh_history' >> ~/.zshrc
+  echo 'HISTSIZE=10000' >> ~/.zshrc
+  echo 'SAVEHIST=10000' >> ~/.zshrc
+  echo 'setopt appendhistory' >> ~/.zshrc
 else
-   echo "pw10k already installed, skipped" >&2
+  echo "pw10k already installed, skipped" >&2
 fi
-
-# common apps
-yay -S --noconfirm google-chrome notion-app 1password whatsapp-nativefier zoom slack-desktop
-
-# dev apps
-yay -S --noconfirm visual-studio-code-bin alacritty neovim
 
 # asdf
 if ! [ -x "$(command -v asdf)" ]; then
-    yay -S --noconfirm asdf-vm
-    echo 'source /opt/asdf-vm/asdf.sh' >> ~/.zshrc
-    asdf plugin add nodejs
-    asdf install nodejs latest
-    echo "asdf installed successfully" >&2
+  yay -S --noconfirm asdf-vm
+  echo 'source /opt/asdf-vm/asdf.sh' >> ~/.zshrc
+  asdf plugin add nodejs
+  asdf install nodejs latest
+  echo "asdf installed successfully" >&2
 else
-    echo "asdf already installed, skipped" >&2
+  echo "asdf already installed, skipped" >&2
 fi
 
 # snap
 if ! [ -x "$(command -v snap)" ]; then
-    yay -S --noconfirm snapd
-    systemctl enable --now snapd.service
-    echo "snap installed successfully" >&2
-else
-    echo "snap already installed, skipped" >&2
-fi
+  yay -S --noconfirm snapd
+  systemctl enable --now snapd.service
 
-# installing snap packages
-snap install spotify
+  # installing snap packages
+  snap install spotify
 
-# allacritty theme
-if ! [ -d "$HOME/.alacritty-colorscheme" ]; then
-    git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
-    echo 'import:' >> ~/.alacritty.yaml
-    echo '  - ~/.alacritty-colorscheme/themes/gruvbox_material.yaml' >> ~/.alacritty.yml
-    echo "allacritty theme installed" >&2
+  echo "snap installed successfully" >&2
 else
-   echo "allacritty theme already installed, skipped" >&2
+  echo "snap already installed, skipped" >&2
 fi
 
 # nvim config
 if ! [ -d "$HOME/.config/nvim" ]; then
-    git clone https://github.com/luanbitar/nvim.git ~/.config/nvim
-    echo "nvim config theme installed" >&2
+  git clone https://github.com/luanbitar/nvim.git ~/.config/nvim
+  echo "nvim config theme installed" >&2
 else
-   echo "nvim config theme already installed, skipped" >&2
+  echo "nvim config theme already installed, skipped" >&2
 fi
