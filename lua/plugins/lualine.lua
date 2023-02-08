bffd815a92d82435330ed5f4acc905089c637708
Luan Bitar <7@lbitar.com>
2023-02-08 17:40:56 -0300

    feat: adding lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,5 +8,14 @@ local theme = require "lualine.themes.gruvbox-material"
 theme.normal.a.bg = "#FFFFFF"
 
 lualine.setup({
-  options = { theme = theme }
+  options = { 
+    theme = theme,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+  },
+  sections = {
+    lualine_x = {"filetype"},
+    lualine_y = {}
+  }
 })
+
