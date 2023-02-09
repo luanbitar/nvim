ae106168eadccb591ad168487ea9e9293f182e32
Luan Bitar <7@lbitar.com>
2023-02-09 14:08:17 -0300

    feat: removing empty line

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
@@ -21,78 +21,77 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  use "wbthomason/packer.nvim" -- plugins manager
-  use "nvim-lua/plenary.nvim" -- lua functions to other plugins
-  use "sainnhe/gruvbox-material" -- theme
+	use("wbthomason/packer.nvim") -- plugins manager
+	use("nvim-lua/plenary.nvim") -- lua functions to other plugins
+	use("sainnhe/gruvbox-material") -- theme
 
-  -- navigation plugins
-  use "christoomey/vim-tmux-navigator" -- split and tmux navigator
-  use "szw/vim-maximizer" -- maximing an split
+	-- navigation plugins
+	use("christoomey/vim-tmux-navigator") -- split and tmux navigator
+	use("szw/vim-maximizer") -- maximing an split
 
-  -- essential plugins
-  use "tpope/vim-surround" -- surrounding text shortcuts
-  use "vim-scripts/ReplaceWithRegister" -- replace sections with clipboard
-  use "numToStr/Comment.nvim" -- commenting plugin
-  use "nvim-tree/nvim-tree.lua" -- file explorer
-  use "kyazdani42/nvim-web-devicons" -- file icons
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+	-- essential plugins
+	use("tpope/vim-surround") -- surrounding text shortcuts
+	use("vim-scripts/ReplaceWithRegister") -- replace sections with clipboard
+	use("numToStr/Comment.nvim") -- commenting plugin
+	use("nvim-tree/nvim-tree.lua") -- file explorer
+	use("kyazdani42/nvim-web-devicons") -- file icons
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-  -- telescope
-  use {"nvim-telescope/telescope-fzf-native.nvim", run = "make" }
-  use {
-    "nvim-telescope/telescope.nvim", tag = "0.1.1",
-    requires = { {"nvim-lua/plenary.nvim"} }
-  }
+	-- telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  -- autocompletion
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
 
-  -- snippets
-  use "L3MON4D3/LuaSnip"
-  use "saadparwaiz1/cmp_luasnip"
-  use "rafamadriz/friendly-snippets"
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
-  -- lsp
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig"
-  use "neovim/nvim-lspconfig"
-  use "hrsh7th/cmp-nvim-lsp"
-  use {"glepnir/lspsaga.nvim", branch = "main"}
-  use "jose-elias-alvarez/typescript.nvim"
-  use "onsails/lspkind.nvim"
+	-- lsp
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("jose-elias-alvarez/typescript.nvim")
+	use("onsails/lspkind.nvim")
 
-  -- formatting and linting
-  use "jose-elias-alvarez/null-ls.nvim"
-  use "jayp0521/mason-null-ls.nvim"
+	-- formatting and linting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
 
-  -- treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-  }
+	-- treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-  -- auto closing
-  use "windwp/nvim-autopairs"
-  use "windwp/nvim-ts-autotag"
+	-- auto closing
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
 
-  -- git signs
-  use "lewis6991/gitsigns.nvim"
+	-- git signs
+	use("lewis6991/gitsigns.nvim")
 
-
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
