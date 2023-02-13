67cefb753911d7c12c56c70b90b1f4de4727cf62
Luan Bitar <7@lbitar.com>
2023-02-13 10:01:05 -0300

    feat: replacing sumneko lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,55 +1,55 @@
 local status_mason, mason = pcall(require, "mason")
 if not status_mason then
-  return
+	return
 end
 
 local status_lspconfig, lspconfig = pcall(require, "mason-lspconfig")
 if not status_lspconfig then
-  return
+	return
 end
 
 local setup_mason_null_ls, mason_null_ls = pcall(require, "mason-null-ls")
 if not setup_mason_null_ls then
-  return
+	return
 end
 
 mason.setup()
 
 lspconfig.setup({
-  ensure_installed = {
-    "arduino_language_server",
-    "bashls",
-    "cssls",
-    "cssmodules_ls",
-    "eslint",
-    "emmet_ls",
-    "graphql",
-    "html",
-    "jsonls",
-    "quick_lint_js",
-    "tsserver",
-    -- "vtsls",
-    "sumneko_lua",
-    "marksman",
-    "prosemd_lsp",
-    "remark_ls",
-    "zk",
-    "prismals",
-    "rust_analyzer",
-    "sqlls",
-    -- "sqls",
-    "stylelint_lsp",
-    "svelte",
-    "tailwindcss",
-    "lemminx",
-    "yamlls",
-  }
+	ensure_installed = {
+		"arduino_language_server",
+		"bashls",
+		"cssls",
+		"cssmodules_ls",
+		"eslint",
+		"emmet_ls",
+		"graphql",
+		"html",
+		"jsonls",
+		"quick_lint_js",
+		"tsserver",
+		-- "vtsls",
+		"lua_ls",
+		"marksman",
+		"prosemd_lsp",
+		"remark_ls",
+		"zk",
+		"prismals",
+		"rust_analyzer",
+		"sqlls",
+		-- "sqls",
+		"stylelint_lsp",
+		"svelte",
+		"tailwindcss",
+		"lemminx",
+		"yamlls",
+	},
 })
 
 mason_null_ls.setup({
-  ensure_installed = {
-    "prettier",
-    "eslint_d",
-    "stylua",
-  },
+	ensure_installed = {
+		"prettier",
+		"eslint_d",
+		"stylua",
+	},
 })
